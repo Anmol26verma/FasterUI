8602b49b66977f15dab92db7a34615ceee52fa1e
Anmol26verma <vermaanmol807@gmail.com>
2024-05-30 16:49:31 +0530

    Updation

---
+++ aap.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const menuToggle = document.createElement('div');
+//     menuToggle.classList.add('menu-toggle');
+//     menuToggle.innerHTML = '<span></span><span></span><span></span>';
+//     document.querySelector('.logo').appendChild(menuToggle);
+
+//     menuToggle.addEventListener('click', () => {
+//         document.querySelector('.navbar-menu').classList.toggle('active');
+//     });
+// });
